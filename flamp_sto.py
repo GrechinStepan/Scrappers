65ea9537a5fbe2ec20c325a188fe61c424ff0aff
Grechin Stepan <100774361+GrechinStepan@users.noreply.github.com>
2022-03-16 15:58:05 +0300

    Update flamp_sto.py

--- flamp_sto.py
+++ flamp_sto.py
@@ -16,12 +16,12 @@ def get_data(url, page, city_rus):
     for p in range(1, page):
         print(f'Обрабатывается страница № {p}')
         headers = {
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+                'user-agent': '',
                 }
         
 
         proxies = {
-                'http': 'http://109.172.110.232:49155',
+                'http': '',
                 }
                     
         req = requests.get(url + f'{p}', headers = headers, proxies=proxies).text
